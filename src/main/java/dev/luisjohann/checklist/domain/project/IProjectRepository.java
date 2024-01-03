e98f2ba6046460f57f088277ee4fd490f9a6325c
luis <luisfj_pr@hotmail.com>
2024-01-03 16:46:04 +0000

    :recycle: refactor: Remove unnecessary throws

--- src/main/java/dev/luisjohann/checklist/domain/project/IProjectRepository.java
+++ src/main/java/dev/luisjohann/checklist/domain/project/IProjectRepository.java
@@ -1,11 +1,10 @@
 package dev.luisjohann.checklist.domain.project;
 
-import dev.luisjohann.checklist.domain.project.exceptions.ProjectNotFoundException;
 import reactor.core.publisher.Mono;
 
 public interface IProjectRepository {
 
     Mono<Project> createProject(Project project);
 
-    Mono<Project> findBySlug(String slug) throws ProjectNotFoundException;
+    Mono<Project> findBySlug(String slug);
 }
