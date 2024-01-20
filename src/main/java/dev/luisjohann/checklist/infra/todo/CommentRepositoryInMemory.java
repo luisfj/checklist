aacefb02a876c788810cb8f4d78b502007c3dc73
luis <luisfj_pr@hotmail.com>
2024-01-20 17:48:56 +0000

    :sparkles: feat: Add in memory comment repository

---
+++ src/main/java/dev/luisjohann/checklist/infra/todo/CommentRepositoryInMemory.java
@@ -0,0 +1,51 @@
+package dev.luisjohann.checklist.infra.todo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import dev.luisjohann.checklist.domain.todo.Comment;
+import dev.luisjohann.checklist.domain.todo.ICommentRepository;
+import dev.luisjohann.checklist.domain.todo.exception.CommentNotFoundException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class CommentRepositoryInMemory implements ICommentRepository {
+
+    private List<Comment> comments = new ArrayList<>();
+
+    @Override
+    public Mono<Comment> createComment(Comment comment) {
+        this.comments.add(comment);
+        return Mono.just(comment);
+    }
+
+    @Override
+    public Mono<Comment> updateComment(Comment comment) {
+        var idx = this.comments.indexOf(comment);
+
+        if (idx < 0 || this.comments.get(idx).todo().equals(comment.todo())) {
+            return Mono.error(new CommentNotFoundException(comment.id(), comment.todo().getId()));
+        }
+
+        this.comments.set(idx, comment);
+
+        return Mono.just(comment);
+    }
+
+    @Override
+    public Mono<Comment> findByIdAndTodoId(String id, String todoId) {
+        var comment = this.comments.stream()
+                .filter(c -> c.id().equals(id) && c.todo().getId().equals(todoId)).findFirst()
+                .orElse(null);
+        return Objects.isNull(comment) ? Mono.empty() : Mono.just(comment);
+    }
+
+    @Override
+    public Flux<Comment> listAllCommentsByTodoId(String todoId) {
+        return Flux.fromIterable(
+                this.comments.stream().filter(c -> c.todo().getId().equals(todoId)).collect(Collectors.toList()));
+    }
+
+}
--- src/test/java/dev/luisjohann/checklist/infra/configuration/TestAppConfig.java
+++ src/test/java/dev/luisjohann/checklist/infra/configuration/TestAppConfig.java
@@ -7,9 +7,11 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 import dev.luisjohann.checklist.domain.project.IProjectRepository;
 import dev.luisjohann.checklist.domain.project.ISlugGenerator;
 import dev.luisjohann.checklist.domain.project.IWorkerRepository;
+import dev.luisjohann.checklist.domain.todo.ICommentRepository;
 import dev.luisjohann.checklist.infra.project.ProjectRepositoryInMemory;
 import dev.luisjohann.checklist.infra.project.SlugGeneratorTest;
 import dev.luisjohann.checklist.infra.project.WorkerRepositoryInMemory;
+import dev.luisjohann.checklist.infra.todo.CommentRepositoryInMemory;
 
 @TestConfiguration
 @EnableWebFlux
@@ -28,6 +30,11 @@ public class TestAppConfig {
     @Bean
     public IWorkerRepository workerRepository() {
         return new WorkerRepositoryInMemory();
+    }
+
+    @Bean
+    public ICommentRepository commentRepository() {
+        return new CommentRepositoryInMemory();
     }
 
 }
