c03de3dc5bbba61cfdf7016fa6781707db8e1d8e
luis <luisfj_pr@hotmail.com>
2024-03-14 11:51:53 +0000

    :sparkles: feat: Change beans to slugify and Project Repo Jpa

--- checklist/src/main/java/dev/luisjohann/checklist/infra/configuration/AppConfig.java
+++ checklist/src/main/java/dev/luisjohann/checklist/infra/configuration/AppConfig.java
@@ -2,6 +2,8 @@ package dev.luisjohann.checklist.infra.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 import dev.luisjohann.checklist.domain.project.IProjectRepository;
@@ -9,23 +11,27 @@ import dev.luisjohann.checklist.domain.project.ISlugGenerator;
 import dev.luisjohann.checklist.domain.project.IWorkerRepository;
 import dev.luisjohann.checklist.domain.todo.ICommentRepository;
 import dev.luisjohann.checklist.domain.todo.ITodoRepository;
-import dev.luisjohann.checklist.infra.project.ProjectRepositoryInMemory;
-import dev.luisjohann.checklist.infra.project.SlugGeneratorTest;
-import dev.luisjohann.checklist.infra.project.WorkerRepositoryInMemory;
+import dev.luisjohann.checklist.infra.project.repository.jpa.ProjectRepositoryJpa;
+import dev.luisjohann.checklist.infra.project.repository.jpa.ProjectRepositoryJpaImpl;
+import dev.luisjohann.checklist.infra.project.repository.memory.WorkerRepositoryInMemory;
+import dev.luisjohann.checklist.infra.project.slug.slugify.SlugifyGenerator;
 import dev.luisjohann.checklist.infra.todo.CommentRepositoryInMemory;
 import dev.luisjohann.checklist.infra.todo.TodoRepositoryInMemory;
 
 @Configuration
 @EnableWebFlux
+@EnableJpaRepositories(basePackages = "dev.luisjohann.checklist.infra.project.repository.jpa")
+@EnableTransactionManagement
 public class AppConfig {
+
     @Bean
-    public IProjectRepository projectRepository() {
-        return new ProjectRepositoryInMemory();
+    public IProjectRepository projectRepository(final ProjectRepositoryJpa repoJpa) {
+        return new ProjectRepositoryJpaImpl(repoJpa);
     }
 
     @Bean
     public ISlugGenerator slugGenerator() {
-        return new SlugGeneratorTest();
+        return new SlugifyGenerator();
     }
 
     @Bean
